58b71149e3b77599e65bfabcf38af2235bc8c6f3
ss249tp <ss249.tp@gmail.com>
2025-01-07 23:28:07 +0800

    docs: Set up MkDocs

--- src/my_mock_pkg/example.py
+++ src/my_mock_pkg/example.py
@@ -1,2 +1,10 @@
 def add_one(number: float) -> float:
+    """Increment number by 1.
+
+    Args:
+        number: Value to increment.
+
+    Returns:
+        Value incremented by 1.
+    """
     return number + 1
